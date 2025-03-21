08593d505dca563e164cf8cf2f61ebadef379395
nMaax <massimilianocarli@proton.me>
2025-03-21 19:58:36 +0100

    refactored data loader and train wrapper

--- utils/train_wrapper.py
+++ utils/train_wrapper.py
@@ -2,18 +2,14 @@ import torch
 import matplotlib.pyplot as plt
 from utils.DataLoader import get_batch
 
-def train_model(model, train_data, test_data, optimizer, epochs, eval_every=1000, batch_size=32, device=None):
-  if device is None:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-  model.to(device)  # Move model to the specified device
+def train_model(model, train_data, test_data, optimizer, epochs, batch_size, block_size, device, eval_every=1000):
 
   train_loss_values = []
   test_loss_values = []
 
   for i in range(epochs):
     # Training
-    xb, yb = get_batch(data=train_data, device=device, batch_size=batch_size)
+    xb, yb = get_batch(data=train_data, batch_size=batch_size, block_size=block_size, device=device)
     loss = model.compute_loss(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
@@ -23,7 +19,7 @@ def train_model(model, train_data, test_data, optimizer, epochs, eval_every=1000
     # Evaluation
     if i % eval_every == 0:
       with torch.no_grad():
-        xb, yb = get_batch(data=test_data, device=device)
+        xb, yb = get_batch(data=test_data, batch_size=batch_size, block_size=block_size, device=device)
         test_loss = model.compute_loss(xb, yb)
         test_loss_values.append(test_loss.item())
         print(f"Epoch {i}: Train Loss = {loss.item():.4f}, Test Loss = {test_loss.item():.4f}")
